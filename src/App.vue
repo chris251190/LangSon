64c0adf74b122b90b12525b22f8592aa72f1d5d7
Christian Lehr <christian_lehr.1@gmx.de>
2019-02-16 21:27:43 +0100

    add translator

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
-  <div id="app">
-    <LangSon/>
-  </div>
+    <div id="app">
+        <LangSon/>
+        <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
+        <TranslateOutput v-text="translatedText"></TranslateOutput>
+    </div>
 </template>
 
 <script>
-import LangSon from './components/LangSon.vue'
+    /* eslint-disable no-console */
 
-export default {
-  name: 'app',
-  components: {
-    LangSon
-  }
-}
+    import LangSon from './components/LangSon.vue'
+    import TranslateForm from './components/TranslateForm.vue'
+    import TranslateOutput from './components/TranslateOutput.vue'
+
+    export default {
+        name: 'app',
+        components: {
+            LangSon,
+            TranslateForm,
+            TranslateOutput
+        },
+        data: function () {
+            return {
+                translatedText: ''
+            }
+        },
+        methods: {
+            translateText: function (text, language) {
+                this.$http.get('https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20170329T180255Z.3c18d2dc7b65d525.f23ed9a9efa992bded4ef96334e3c154f61d2dea&lang=' + language + '&text=' + text)
+                    .then((response) => {
+                        this.translatedText = response.body.text[0];
+                    });
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,40 @@
+<template>
+      <form id="transForm" v-on:submit="formSubmit">
+        <input type="text" v-model="textToTranslate" placeholder="Enter a Word...">
+        <select v-model="language">
+          <option value="ru">Russian</option>
+          <option value="es">Spanish</option>
+          <option value="fr">French</option>
+          <option value="zh">Chinese</option>
+        </select>
+        <input type="submit" value="Translate">
+      </form>
+</template>
+
+<script>
+export default {
+  name: 'translateForm',
+  data(){
+    return {
+      textToTranslate:'',
+      language:''
+    }
+  },
+  created() {
+    this.language = 'ru';
+  },
+  methods: {
+    formSubmit(e){
+      this.$emit('formSubmit', this.textToTranslate, this.language);
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+  #transForm{
+    border-radius:10px;
+    border: 1px #ccc solid;
+  }
+</style>
