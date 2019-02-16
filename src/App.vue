bbd168f745b1b59501fc00ecc4398414c678b819
Christian Lehr <christian_lehr.1@gmx.de>
2019-02-16 20:47:34 +0100

    setup project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LangSon/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LangSon from './components/LangSon.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    LangSon
   }
 }
 </script>
---
+++ src/components/LangSon.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>Welcome to LangSon!</h1>
+        <p contenteditable='true'>(Paste your song in here and add the translations of your mother language)</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'LangSon'
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
